bc155e9eddfbdabaa4e5d5a5f1d690ccfa7d45cf
Adam Powers <apowers@ato.ms>
2024-09-07 07:27:44 -0700

    refactor apis

--- cudaffi/module.py
+++ cudaffi/module.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 """CUDA Source Modules"""
 
 import ctypes
-from typing import Any, NewType
+from typing import Any, NewType, Protocol
 
 import numpy as np
 from cuda import cuda, nvrtc
 
-from .core import CudaDevice, CudaStream
+from .core import CudaDevice, CudaStream, init
 from .memory import CudaMemory, NvMemory
 from .utils import checkCudaErrors
 
@@ -28,26 +28,22 @@ NvKernelArgs = (
 )
 
 
-def make_args(args: KernelArgs) -> NvKernelArgs:
-    if args is None:
-        nv_args: NvKernelArgs | int = 0
-    else:
-        if isinstance(args, CudaData):
-            args = [args]
-
-        nv_data_args = tuple(arg.data for arg in args)
-        nv_type_args = tuple(arg.type for arg in args)
-        nv_args = (nv_data_args, nv_type_args)
-    return nv_args
+class CudaDataException(Exception):
+    pass
 
 
 class CudaData:
     def __init__(self, data: int | CudaMemory, datatype: NvDataType | None = None) -> None:
-        if isinstance(data, int):
-            self.data: int | NvMemory = data
-        if isinstance(data, CudaMemory):
-            self.data = data.nv_memory
-            datatype = ctypes.c_void_p
+        match data:
+            case int():
+                print("data is int")
+                self.data: int | NvMemory = data
+            case CudaMemory():
+                print("data is CudaMemory")
+                self.data = data.nv_memory
+                datatype = ctypes.c_void_p
+            case _:
+                raise CudaDataException(f"can't convert data to CudaData: '{data}'")
 
         if datatype is None:
             datatype = ctypes.c_uint
@@ -57,13 +53,85 @@ class CudaData:
 KernelArgs = CudaData | list[CudaData] | None
 
 
+class GridSpecCallback(Protocol):
+    def __call__(self, name: str, mod: CudaModule, *args: Any) -> GridSpec: ...
+
+
+class BlockSpecCallback(Protocol):
+    def __call__(self, name: str, mod: CudaModule, *args: Any) -> BlockSpec: ...
+
+
 class CudaFunction:
-    def __init__(self, src: CudaModule, name: str) -> None:
-        self.src = src
-        self.name = name
-        self.nv_kernel: NvKernel = checkCudaErrors(
-            cuda.cuModuleGetFunction(src.nv_module, name.encode())
+    def __init__(self, mod: CudaModule, name: str) -> None:
+        self.__cuda_module__ = mod
+        self.__name__ = name
+        self.__default_grid__: GridSpecCallback | GridSpec = (1, 1, 1)
+        self.__default_block__: BlockSpecCallback | BlockSpec = (1, 1, 1)
+        self.__nv_kernel__: NvKernel = checkCudaErrors(
+            cuda.cuModuleGetFunction(mod.nv_module, name.encode())
         )
+
+    def __call__(self, *args: Any, stream: CudaStream | None = None) -> None:
+        print("__call__ args", args)
+        # name: str,
+        # args: KernelArgs = None,
+        # *,
+        # block: BlockSpec = (1, 1, 1),
+        # grid: GridSpec = (1, 1, 1),
+        # stream: CudaStream | None = None,
+
+        if stream is None:
+            stream = CudaStream.get_default()
+
+        print(f"Calling function: {self.__name__} with args: {args}")  # noqa: T201
+
+        nv_args = CudaFunction.make_args(*args)
+
+        print("nv_args", nv_args)
+
+        if isinstance(self.__default_grid__, tuple):
+            grid = self.__default_grid__
+        else:
+            grid = self.__default_grid__(self.__name__, self.__cuda_module__, *args)
+
+        if isinstance(self.__default_block__, tuple):
+            block = self.__default_block__
+        else:
+            block = self.__default_block__(self.__name__, self.__cuda_module__, *args)
+
+        checkCudaErrors(
+            cuda.cuLaunchKernel(
+                self.__nv_kernel__,
+                grid[0],  # grid x dim
+                grid[1],  # grid y dim
+                grid[2],  # grid z dim
+                block[0],  # block x dim
+                block[1],  # block y dim
+                block[2],  # block z dim
+                0,  # dynamic shared memory
+                stream.nv_stream,  # stream
+                #    args.ctypes.data,  # kernel arguments
+                nv_args,  # kernel arguments
+                0,  # extra (ignore)
+            )
+        )
+
+    @staticmethod
+    def make_args(*args: Any) -> NvKernelArgs:
+        if len(args) == 0:
+            return 0
+
+        converted_args: list[CudaData] = []
+        for arg in args:
+            if isinstance(arg, CudaData):
+                converted_args.append(arg)
+            else:
+                converted_args.append(CudaData(arg))
+
+        nv_data_args = tuple(arg.data for arg in converted_args)
+        nv_type_args = tuple(arg.type for arg in converted_args)
+        nv_args = (nv_data_args, nv_type_args)
+        return nv_args
 
 
 class CudaModule:
@@ -71,15 +139,23 @@ class CudaModule:
 
     # TODO: include paths, compiler flags
     def __init__(
-        self, code: str, *, no_extern: bool = False, progname: str = "<unspecified>"
+        self,
+        code: str,
+        *,
+        no_extern: bool = False,
+        progname: str = "<unspecified>",
+        device: CudaDevice | None = None,
+        stream: CudaStream | None = None,
     ) -> None:
-        device = CudaDevice.default()
-        context = device.default_context  # cuModuleLoadData requires a context
-        stream = device.default_stream
+        init()
+
+        if device is None:
+            device = CudaDevice.get_default()
+
         self.progname = progname
         if not no_extern:
             self.code = 'extern "C" {\n' + code + "\n}\n"
-        print(f"CODE:\n-------\n{self.code}\n-------\n")
+        print(f"CODE:\n-------\n{self.code}\n-------\n")  # noqa: T201
 
         # Create program
         self.nv_prog: NvProgram = checkCudaErrors(
@@ -97,9 +173,9 @@ class CudaModule:
         checkCudaErrors(nvrtc.nvrtcGetProgramLog(self.nv_prog, buf))
         self.compile_log = buf.decode()
         if log_sz > 1:
-            print(f"Compilation results ({log_sz} bytes):\b{self.compile_log}")
+            print(f"Compilation results ({log_sz} bytes): {self.compile_log}")  # noqa: T201
         else:
-            print("Compilation complete, no warnings.")
+            print("Compilation complete, no warnings.")  # noqa: T201
         checkCudaErrors(compile_result)
 
         # Get PTX from compilation
@@ -107,59 +183,20 @@ class CudaModule:
         self.ptx = b" " * self.nv_ptx_size
         checkCudaErrors(nvrtc.nvrtcGetPTX(self.nv_prog, self.ptx))
 
-    def get_function(
-        self,
-        name: str,
-        *,
-        device: CudaDevice | None = None,
-    ) -> CudaFunction:
-        if device is None:
-            device = CudaDevice.default()
-        context = device.default_context  # cuModuleLoadData requires a context
-        stream = device.default_stream
+    def get_function(self, name: str) -> CudaFunction:
+        init()
 
         # Load PTX as module data and retrieve function
         self.ptx = np.char.array(self.ptx)
+        print("TODO: better checking here")
         self.nv_module = checkCudaErrors(cuda.cuModuleLoadData(self.ptx.ctypes.data))
 
+        print("returning cuda function")
         return CudaFunction(self, name)
 
-    def call(
-        self,
-        name: str,
-        args: KernelArgs = None,
-        *,
-        block: BlockSpec = (1, 1, 1),
-        grid: GridSpec = (1, 1, 1),
-        stream: CudaStream | None = None,
-    ) -> None:
-        if stream is None:
-            device = CudaDevice.default()
-            stream = device.default_stream
-
-        print("Calling function:", name)
-        fn = self.get_function(name, device=device)
-
-        nv_args = make_args(args)
-
-        print("nv_args", nv_args)
-
-        checkCudaErrors(
-            cuda.cuLaunchKernel(
-                fn.nv_kernel,
-                grid[0],  # grid x dim
-                grid[1],  # grid y dim
-                grid[2],  # grid z dim
-                block[0],  # block x dim
-                block[1],  # block y dim
-                block[2],  # block z dim
-                0,  # dynamic shared memory
-                stream.nv_stream,  # stream
-                #    args.ctypes.data,  # kernel arguments
-                nv_args,  # kernel arguments
-                0,  # extra (ignore)
-            )
-        )
+    def __getattr__(self, name: str) -> CudaFunction:
+        print("__getattr__")
+        return self.get_function(name)
 
     # def __del__(self) -> None:
     #     checkCudaErrors(cuda.cuModuleUnload(self.nv_module))
