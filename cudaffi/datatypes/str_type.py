68f3cc0cb173162b3343733d3f364065a4806373
Adam Powers <apowers@ato.ms>
2024-09-12 22:03:37 -0700

    add str datatype

--- cudaffi/datatypes/str_type.py
+++ cudaffi/datatypes/str_type.py
@@ -1,20 +1,18 @@
-# mod = CudaSourceFile("print_buf.cu")
-# str = bytearray(b"hi there.")
-# str.append(0)
-# arr = np.array(str, dtype=np.uint8)
-# mem = CudaMemory.from_np(arr)
-# print("mem hex", hex(mem.nv_memory))
-# mod.call("print_buf", [CudaData(mem), CudaData(mem.size)])
-
-import ctypes
 from typing import Any
 
-from .base import AnyCType, CudaDataType
+from cuda import cuda
+
+from ..memory import CudaDataType, CudaDeviceMemory
+from ..utils import checkCudaErrors
 
 
 class CudaStrDataType(CudaDataType):
-    def convert(self, data: Any, name: str) -> tuple[int, AnyCType] | None:
+    def convert(self, name: str, data: Any) -> CudaDeviceMemory | None:
         if isinstance(data, str):
-            return (0, ctypes.c_uint)
+            s = bytearray(data.encode())
+            s.append(0)
+            mem = CudaDeviceMemory(len(s))
+            checkCudaErrors(cuda.cuMemcpyHtoD(mem.nv_device_memory, s, len(s)))
+            return mem
 
         return None
