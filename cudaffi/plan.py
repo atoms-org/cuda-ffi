a4ea4b2f5e13825e45eb5fbf6e55169b84e9d993
Adam Powers <apowers@ato.ms>
2024-10-03 20:58:22 -0700

    add CudaPlan kwargs, constants, decorator

--- cudaffi/plan.py
+++ cudaffi/plan.py
@@ -1,45 +1,46 @@
 import ast
 import inspect
 import textwrap
-import time
-from functools import partial, wraps
-from typing import Any, Callable
+from enum import Enum, auto
+from functools import update_wrapper
+from typing import Any, Callable, ParamSpec, TypeVar, cast
 
 from cudaffi.module import CudaFunction, CudaModule
+
+from .graph.graph import CudaGraph
 
 ModType = dict[str, str | CudaModule]
 AnyFn = Callable[[Any], Any]
 
+P = ParamSpec("P")
+R = TypeVar("R")
 
-def cuda_plan(
-    func: AnyFn | None = None, *, seconds: int | None = None, msg: str | None = None
-) -> AnyFn:
-    if func is None:
-        return partial(cuda_plan, seconds=seconds, msg=msg)
+# def cuda_plan() -> Callable[..., Any]:
+#     def decorator(fn: Callable[..., Any]) -> Callable[..., Any]:
+#         @wraps(fn)
+#         def wrapper() -> CudaPlan:
+#             return CudaPlan(fn)
 
-    seconds = seconds if seconds else 1
-    msg = msg if msg else "Sleeping for {} seconds".format(seconds)
+#         return wrapper
 
-    @wraps(func)
-    def wrapper(*args: list[Any], **kwargs: dict[str, Any]) -> Any:
-        print(msg)
-        print("before")
-        time.sleep(seconds)
-        ret = func(*args, **kwargs)
-        print("after")
-        return ret
+#     return decorator
 
-    return wrapper
+
+def cuda_plan(fn: Callable[P, R]) -> Callable[P, R]:
+    return cast(Callable[P, R], CudaPlan(fn))
 
 
 class CudaPlanException(Exception):
-    pass
+    def __init__(self, message: str) -> None:
+        self.message = message
 
 
 class CudaPlan:
     def __init__(self, fn: Callable[..., Any]) -> None:
         self.src_text = textwrap.dedent(inspect.getsource(fn))
         self.src_ast = ast.parse(self.src_text)
+
+        update_wrapper(self, fn)
 
         # source should be a single function
         if len(self.src_ast.body) != 1:
@@ -70,8 +71,18 @@ class CudaPlan:
             # TODO: new CudaPlanVar
 
         if self.fn_def_ast.returns is not None:
-            assert isinstance(self.fn_def_ast.returns, ast.Name)
-            self.output_type = self.fn_def_ast.returns.id
+            match self.fn_def_ast.returns:
+                case ast.Name():
+                    self.output_type = self.fn_def_ast.returns.id
+                case ast.Constant():
+                    self.output_type = str(self.fn_def_ast.returns.value)
+                    print("output type constant:", self.output_type)
+                case ast.Subscript():
+                    self.output_type = ast.unparse(self.fn_def_ast.returns)
+                case _:
+                    raise CudaPlanException(
+                        f"unknown return type: '{self.fn_def_ast.returns.__class__.__name__}'"
+                    )
         else:
             self.output_type = "Any"
 
@@ -80,7 +91,7 @@ class CudaPlan:
         no_more = False
         for stmt in self.fn_def_ast.body:
             if no_more:
-                raise Exception("statements found after return in cuda plan")
+                raise CudaPlanException("statements found after return in CudaPlan")
 
             step = CudaPlanStep(stmt)
             self.steps.append(step)
@@ -88,74 +99,96 @@ class CudaPlan:
             if step.is_return:
                 no_more = True
 
+    def __call__(self, *args: Any, **kwargs: Any) -> None:
+        print("calling CudaPlan")
+
+    def to_graph(self) -> CudaGraph:
+        g = CudaGraph()
+        # TODO: for each statement...
+        return g
+
+
+class CudaPlanVarType(Enum):
+    constant = auto()
+    arg = auto()
+    kwarg = auto()
+
 
 class CudaPlanVar:
-    def __init__(self, name: str, type: str | None = None):
+    def __init__(self, name: str, type: CudaPlanVarType, val: Any = None):
         self.name = name
+        self.type = type
+        self.val = val
 
 
 class CudaPlanStep:
     def __init__(self, stmt: ast.stmt) -> None:
         self.exec: CudaFunction | CudaPlan | None = None
         self.input_vars: list[CudaPlanVar] = []
+        self.input_kwvars: dict[str, CudaPlanVar] = {}
         self.output_vars: list[CudaPlanVar] = []
         self.is_return: bool = False
         self.call_name: str | None = None
         self.call_module: str | None = None
-        self.input_arg_names: list[str] = []
-        self.output_arg_names: list[str] = []
 
         match stmt:
             case ast.Assign():
-                print("statement type: assignment")
-                self.output_arg_names = _decode_assignment_targets(stmt)
+                self.output_vars = _decode_assignment_targets(stmt)
                 assert isinstance(stmt.value, ast.Call)
                 self.call_name, self.call_module = _decode_call_name(stmt.value)
-                self.input_arg_names = _decode_call_args(stmt.value)
+                self.input_vars, self.input_kwvars = _decode_call_args(stmt.value)
             case ast.Return():
                 self.is_return = True
                 match stmt.value:
                     case ast.Name():
-                        self.output_arg_names.append(stmt.value.id)
-                        self.input_arg_names.append(stmt.value.id)
+                        self.output_vars.append(CudaPlanVar(stmt.value.id, CudaPlanVarType.arg))
+                        self.input_vars.append(CudaPlanVar(stmt.value.id, CudaPlanVarType.arg))
                     case ast.Tuple():
                         tup: ast.Tuple = stmt.value
                         for e in tup.elts:
                             assert isinstance(e, ast.Name)
-                            self.output_arg_names.append(e.id)
-                            self.input_arg_names.append(e.id)
+                            self.output_vars.append(CudaPlanVar(e.id, CudaPlanVarType.arg))
+                            self.input_vars.append(CudaPlanVar(e.id, CudaPlanVarType.arg))
                     case ast.Call():
                         self.call_name, self.call_module = _decode_call_name(stmt.value)
-                        self.input_arg_names = _decode_call_args(stmt.value)
+                        self.input_vars, self.input_kwvars = _decode_call_args(stmt.value)
+                    case ast.Constant():
+                        self.output_vars.append(
+                            CudaPlanVar(
+                                str(stmt.value.value),
+                                CudaPlanVarType.constant,
+                                val=stmt.value.value,
+                            )
+                        )
                     case _:
-                        raise Exception(f"unknown return value: '{stmt.value.__class__.__name__}'")
-
-                print("statement type: return")
-                # decode_return(stmt)
+                        raise CudaPlanException(
+                            f"unknown return value: '{stmt.value.__class__.__name__}'"
+                        )
             case ast.Expr():
-                print("statement type: expression")
-                print("is call", isinstance(stmt.value, ast.Call))
-                assert isinstance(stmt.value, ast.Call)
+                if not isinstance(stmt.value, ast.Call):
+                    raise CudaPlanException(
+                        f"unknown expression type: '{stmt.value.__class__.__name__}'"
+                    )
                 self.call_name, self.call_module = _decode_call_name(stmt.value)
-                self.input_arg_names = _decode_call_args(stmt.value)
+                self.input_vars, self.input_kwvars = _decode_call_args(stmt.value)
             case _:
-                raise Exception(f"unknown statement type: '{stmt.__class__.__name__}'")
+                raise CudaPlanException(f"unknown statement type: '{stmt.__class__.__name__}'")
 
 
-def _decode_assignment_targets(assn: ast.Assign) -> list[str]:
-    ret: list[str] = []
+def _decode_assignment_targets(assn: ast.Assign) -> list[CudaPlanVar]:
+    ret: list[CudaPlanVar] = []
 
     for tgt in assn.targets:
         match tgt:
             case ast.Name():
-                ret.append(tgt.id)
+                ret.append(CudaPlanVar(tgt.id, CudaPlanVarType.arg))
             case ast.Tuple():
                 tup: ast.Tuple = tgt
                 for e in tup.elts:
                     assert isinstance(e, ast.Name)
-                    ret.append(e.id)
+                    ret.append(CudaPlanVar(e.id, CudaPlanVarType.arg))
             case _:
-                raise Exception(f"unknown assignment target: '{tgt.__class__.__name__}'")
+                raise CudaPlanException(f"unknown assignment target: '{tgt.__class__.__name__}'")
 
     return ret
 
@@ -170,17 +203,45 @@ def _decode_call_name(call: ast.Call) -> tuple[str, str | None]:
             fn_name = call.func.attr
             return (fn_name, mod_name)
         case _:
-            raise Exception(f"unknown call name: '{call.func.__class__.__name__}'")
+            raise CudaPlanException(f"unknown call name: '{call.func.__class__.__name__}'")
 
 
-def _decode_call_args(call: ast.Call) -> list[str]:
-    ret: list[str] = []
+def _decode_call_args(call: ast.Call) -> tuple[list[CudaPlanVar], dict[str, CudaPlanVar]]:
+    args_ret: list[CudaPlanVar] = []
+    kwargs_ret: dict[str, CudaPlanVar] = {}
 
     for arg in call.args:
         match arg:
             case ast.Name():
-                ret.append(arg.id)
+                args_ret.append(CudaPlanVar(arg.id, CudaPlanVarType.arg))
+            case ast.Constant():
+                args_ret.append(
+                    CudaPlanVar(str(arg.value), CudaPlanVarType.constant, val=arg.value)
+                )
             case _:
-                raise Exception(f"unknown call argument: '{arg.__class__.__name__}'")
+                raise CudaPlanException(f"unknown call argument: '{arg.__class__.__name__}'")
 
-    return ret
+    for kwarg in call.keywords:
+        match arg:
+            case ast.Name():
+                key = kwarg.arg
+                assert key is not None
+                if isinstance(kwarg.value, ast.Name):
+                    kwargs_ret[key] = CudaPlanVar(kwarg.value.id, CudaPlanVarType.arg)
+                else:
+                    val_str = ast.unparse(kwarg.value)
+                    kwargs_ret[key] = CudaPlanVar(
+                        val_str, CudaPlanVarType.constant, val=eval(val_str)
+                    )
+            case ast.Constant():
+                key = kwarg.arg
+                assert key is not None
+                kwargs_ret[key] = CudaPlanVar(
+                    str(kwarg.value), CudaPlanVarType.constant, val=kwarg.value
+                )
+            case _:
+                raise CudaPlanException(
+                    f"unknown call keyword argument: '{arg.__class__.__name__}'"
+                )
+
+    return args_ret, kwargs_ret
--- tests/plan_test.py
+++ tests/plan_test.py
@@ -1,20 +1,31 @@
-from cudaffi.plan import CudaPlan, cuda_plan
+import pytest
+
+from cudaffi.plan import CudaPlan, CudaPlanException, CudaPlanVarType, cuda_plan
 
 
 class TestPlan:
     def test_decorator_with_args(self) -> None:
-        @cuda_plan(msg="blah")
-        def myfunc(foo: int) -> int:
-            return 1
-
-        myfunc(3)
-
-    def test_decorator_without_args(self) -> None:
         @cuda_plan
         def myfunc(foo: int) -> int:
-            return 1
+            return foo
 
         myfunc(3)
+
+        # make sure the function is now a CudaPlan
+        assert isinstance(myfunc, CudaPlan)
+        assert hasattr(myfunc, "fn_def_ast")  # type: ignore
+
+        # make sure the wrapper worked
+        assert myfunc.__name__ == "myfunc"
+        assert hasattr(myfunc, "__wrapped__")
+
+    # def test_decorator_without_args(self) -> None:
+    #     @cuda_plan
+    #     def myfunc(foo: int) -> int:
+    #         return foo
+
+    #     myfunc(3)
+    #     print("myfunc is CudaPlan", isinstance(myfunc, CudaPlan))
 
 
 class TestCudaPlan:
@@ -36,6 +47,14 @@ class TestCudaPlan:
         assert p.inputs == [("a", "str"), ("b", "int")]
         assert p.output_type == "str"
 
+    def test_function_definition_return_none(self) -> None:
+        def myfn(a, b) -> None:  # type: ignore
+            bob()  # type: ignore
+
+        p = CudaPlan(myfn)
+
+        assert p.output_type == "None"
+
     def test_call(self) -> None:
         def myfn(a: str, b: int) -> None:
             bob()  # type: ignore
@@ -47,6 +66,21 @@ class TestCudaPlan:
         assert p.steps[0].call_module is None
         assert len(p.steps[0].output_vars) == 0
         assert len(p.steps[0].input_vars) == 0
+        print("output type", p.output_type)
+
+    # TODO: putting a comma between two statements turns it into a tuple
+    # def test_comma(self) -> None:
+    #     def myfn(a: str, b: int) -> None:
+    #         foo(), bar()  # type: ignore
+
+    #     p = CudaPlan(myfn)
+
+    #     assert len(p.steps) == 1
+    #     assert p.steps[0].call_name == "bob"
+    #     assert p.steps[0].call_module is None
+    #     assert len(p.steps[0].output_vars) == 0
+    #     assert len(p.steps[0].input_vars) == 0
+    #     print("output type", p.output_type)
 
     def test_attribute(self) -> None:
         def myfn(a: str, b: int) -> None:
@@ -69,8 +103,10 @@ class TestCudaPlan:
         assert len(p.steps) == 1
         assert p.steps[0].call_name == "bob"
         assert p.steps[0].call_module is None
-        assert p.steps[0].input_arg_names == []
-        assert p.steps[0].output_arg_names == ["x"]
+        assert len(p.steps[0].input_vars) == 0
+        assert len(p.steps[0].output_vars) == 1
+        assert p.steps[0].output_vars[0].name == "x"
+        assert p.steps[0].output_vars[0].type == CudaPlanVarType.arg
         assert len(p.steps[0].input_vars) == 0
 
     def test_dereferencing(self) -> None:
@@ -82,8 +118,12 @@ class TestCudaPlan:
         assert len(p.steps) == 1
         assert p.steps[0].call_name == "bob"
         assert p.steps[0].call_module is None
-        assert p.steps[0].input_arg_names == []
-        assert p.steps[0].output_arg_names == ["x", "y"]
+        assert len(p.steps[0].input_vars) == 0
+        assert len(p.steps[0].output_vars) == 2
+        assert p.steps[0].output_vars[0].name == "x"
+        assert p.steps[0].output_vars[0].type == CudaPlanVarType.arg
+        assert p.steps[0].output_vars[1].name == "y"
+        assert p.steps[0].output_vars[1].type == CudaPlanVarType.arg
         assert len(p.steps[0].input_vars) == 0
 
     def test_call_args(self) -> None:
@@ -95,10 +135,64 @@ class TestCudaPlan:
         assert len(p.steps) == 1
         assert p.steps[0].call_name == "bob"
         assert p.steps[0].call_module is None
-        assert p.steps[0].input_arg_names == ["a", "b"]
-        assert p.steps[0].output_arg_names == []
+        assert len(p.steps[0].input_vars) == 2
+        assert p.steps[0].input_vars[0].name == "a"
+        assert p.steps[0].input_vars[0].type == CudaPlanVarType.arg
+        assert p.steps[0].input_vars[1].name == "b"
+        assert p.steps[0].input_vars[1].type == CudaPlanVarType.arg
         assert len(p.steps[0].output_vars) == 0
-        assert len(p.steps[0].input_vars) == 0
+        assert not p.steps[0].is_return
+
+    def test_call_args_constant(self) -> None:
+        def myfn(a: str, b: int) -> None:
+            bob(42, 3.14, "foo", True)  # type: ignore
+
+        p = CudaPlan(myfn)
+
+        assert len(p.steps) == 1
+        assert p.steps[0].call_name == "bob"
+        assert p.steps[0].call_module is None
+        assert len(p.steps[0].input_vars) == 4
+        assert p.steps[0].input_vars[0].type == CudaPlanVarType.constant
+        assert p.steps[0].input_vars[0].name == "42"
+        assert p.steps[0].input_vars[0].val == 42
+        assert p.steps[0].input_vars[1].type == CudaPlanVarType.constant
+        assert p.steps[0].input_vars[1].name == "3.14"
+        assert p.steps[0].input_vars[1].val == 3.14
+        assert p.steps[0].input_vars[2].type == CudaPlanVarType.constant
+        assert p.steps[0].input_vars[2].name == "foo"
+        assert p.steps[0].input_vars[2].val == "foo"
+        assert p.steps[0].input_vars[3].type == CudaPlanVarType.constant
+        assert p.steps[0].input_vars[3].name == "True"
+        assert p.steps[0].input_vars[3].val == True
+        assert len(p.steps[0].output_vars) == 0
+        assert not p.steps[0].is_return
+
+    def test_call_kwargs(self) -> None:
+        def myfn(a: str, b: int) -> None:
+            bob(a, foo=bar, block=(1, 1, 1), grid=(1, 1, 1))  # type: ignore
+
+        p = CudaPlan(myfn)
+
+        assert len(p.steps) == 1
+        assert p.steps[0].call_name == "bob"
+        assert p.steps[0].call_module is None
+        assert len(p.steps[0].input_vars) == 1
+        assert p.steps[0].input_vars[0].name == "a"
+        assert p.steps[0].input_vars[0].type == CudaPlanVarType.arg
+        assert len(p.steps[0].input_kwvars.keys()) == 3
+        assert "foo" in p.steps[0].input_kwvars
+        assert p.steps[0].input_kwvars["foo"].name == "bar"
+        assert p.steps[0].input_kwvars["foo"].type == CudaPlanVarType.arg
+        assert "block" in p.steps[0].input_kwvars
+        assert p.steps[0].input_kwvars["block"].name == "(1, 1, 1)"
+        assert p.steps[0].input_kwvars["block"].type == CudaPlanVarType.constant
+        assert p.steps[0].input_kwvars["block"].val == (1, 1, 1)
+        assert "grid" in p.steps[0].input_kwvars
+        assert p.steps[0].input_kwvars["grid"].name == "(1, 1, 1)"
+        assert p.steps[0].input_kwvars["grid"].type == CudaPlanVarType.constant
+        assert p.steps[0].input_kwvars["grid"].val == (1, 1, 1)
+        assert len(p.steps[0].output_vars) == 0
         assert not p.steps[0].is_return
 
     def test_return_var(self) -> None:
@@ -111,7 +205,9 @@ class TestCudaPlan:
         assert p.steps[0].call_name is None
         assert p.steps[0].call_module is None
         assert p.steps[0].is_return
-        assert p.steps[0].output_arg_names == ["a"]
+        assert len(p.steps[0].input_vars) == 1
+        assert p.steps[0].input_vars[0].name == "a"
+        assert p.steps[0].input_vars[0].type == CudaPlanVarType.arg
 
     def test_return_call(self) -> None:
         def myfn(a: str, b: int) -> str:
@@ -123,8 +219,8 @@ class TestCudaPlan:
         assert p.steps[0].call_name == "bob"
         assert p.steps[0].call_module is None
         assert p.steps[0].is_return
-        assert p.steps[0].input_arg_names == []
-        assert p.steps[0].output_arg_names == []
+        assert len(p.steps[0].input_vars) == 0
+        assert len(p.steps[0].output_vars) == 0
 
     def test_return_call_with_module(self) -> None:
         def myfn(a: str, b: int) -> str:
@@ -136,8 +232,8 @@ class TestCudaPlan:
         assert p.steps[0].call_name == "bob"
         assert p.steps[0].call_module == "beer"
         assert p.steps[0].is_return
-        assert p.steps[0].input_arg_names == []
-        assert p.steps[0].output_arg_names == []
+        assert len(p.steps[0].input_vars) == 0
+        assert len(p.steps[0].output_vars) == 0
 
     def test_return_multiple(self) -> None:
         def myfn(a: str, b: int) -> tuple[str, int]:
@@ -148,12 +244,38 @@ class TestCudaPlan:
         assert len(p.steps) == 1
         assert p.steps[0].call_name is None
         assert p.steps[0].call_module is None
-        assert p.steps[0].input_arg_names == ["a", "b"]
-        assert p.steps[0].output_arg_names == ["a", "b"]
+        assert len(p.steps[0].input_vars) == 2
+        assert p.steps[0].input_vars[0].name == "a"
+        assert p.steps[0].input_vars[0].type == CudaPlanVarType.arg
+        assert p.steps[0].input_vars[1].name == "b"
+        assert p.steps[0].input_vars[1].type == CudaPlanVarType.arg
+        assert len(p.steps[0].output_vars) == 2
         assert p.steps[0].is_return
+        assert p.output_type == "tuple[str, int]"
 
-    # constant str
-    # constant int
-    # constant float
-    # kwargs
-    # steps after return
+    def test_return_constant(self) -> None:
+        def myfn(a: str, b: int) -> int:
+            return 42
+
+        p = CudaPlan(myfn)
+
+        assert len(p.steps) == 1
+        assert p.steps[0].call_name is None
+        assert p.steps[0].call_module is None
+        assert len(p.steps[0].input_vars) == 0
+        assert len(p.steps[0].output_vars) == 1
+        assert p.steps[0].output_vars[0].name == "42"
+        assert p.steps[0].output_vars[0].type == CudaPlanVarType.constant
+        assert p.steps[0].output_vars[0].val == 42
+        assert p.steps[0].is_return
+        assert p.output_type == "int"
+
+    def test_steps_after_return(self) -> None:
+        def myfn(a: str, b: int) -> int:
+            return 42
+            bob()  # type: ignore
+
+        with pytest.raises(CudaPlanException) as e:
+            CudaPlan(myfn)
+
+        assert e.value.message == "statements found after return in CudaPlan"
