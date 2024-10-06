d204cb1cb0f2fc81d13b321e4254babcca1ffecf
Adam Powers <apowers@ato.ms>
2024-10-06 11:31:36 -0700

    add expanded int types, fix int sizes and ranges

--- cudaffi/datatypes/int_type.py
+++ cudaffi/datatypes/int_type.py
@@ -1,19 +1,46 @@
 import ctypes
+import re
 import warnings
 from collections.abc import Buffer
 from typing import Any, Generator
 
-from ..args import CudaDataType
+from ..args import CudaDataType, CudaDataTypeWarning
+
+
+class CudaIntSizeWarning(CudaDataTypeWarning):
+    pass
 
 
 class CudaIntDataType(CudaDataType[int]):
     default_direction = "input"
-    aliases = ["uint"]
+    aliases = ["uint", "uint8", "uint16", "uint32", "uint64", "int8", "int16", "int32", "int64"]
 
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.signed = not name.startswith("u")
-        self.byte_size = 4
+        self.int_name = name
+
+        # calculate byte size of this flavor of int
+        specified_bits_match = re.search(r"\d+$", name)
+        if specified_bits_match is None:
+            specified_bits = 32
+        else:
+            specified_bits = int(specified_bits_match.group())
+        self.byte_size = specified_bits // 8
+
+        # calculate value range of this flavor of it
+        num_bits = self.byte_size * 8
+        if self.signed:
+            num_bits -= 1
+            max_size = (2**num_bits) - 1
+            min_size = (2**num_bits) * -1
+        else:
+            max_size = (2**num_bits) - 1
+            min_size = 0
+
+        self.num_bits = num_bits
+        self.max_size = max_size
+        self.min_size = min_size
 
     def is_type(self, data: Any) -> bool:
         return isinstance(data, int)
@@ -25,20 +52,17 @@ class CudaIntDataType(CudaDataType[int]):
         return ctypes.c_int
 
     def encode(self, data: int) -> int:
-        num_bits = self.byte_size * 8
-        if self.signed:
-            num_bits -= 1
-            max_size = (2**num_bits) - 1
-            min_size = 2**num_bits
-        else:
-            max_size = (2**num_bits) - 1
-            min_size = 0
+        if data > self.max_size:
+            warnings.warn(
+                f"passed int value '{data}' too large for {self.byte_size} bytes of {self.int_name}",
+                CudaDataTypeWarning,
+            )
 
-        if data > max_size:
-            warnings.warn("int too big for size")
-
-        if data < min_size:
-            warnings.warn("int too small for size")
+        if data < self.min_size:
+            warnings.warn(
+                f"passed int value '{data}' too small for {self.byte_size} bytes of {self.int_name}",
+                CudaDataTypeWarning,
+            )
 
         return data
 
--- tests/args_test.py
+++ tests/args_test.py
@@ -1,6 +1,8 @@
 import ctypes
 
-from cudaffi.args import CudaArg, CudaArgDirection
+import pytest
+
+from cudaffi.args import CudaArg, CudaArgDirection, CudaArgType, CudaDataTypeWarning
 
 
 class TestArgs:
@@ -22,8 +24,76 @@ class TestArgs:
         assert arg.dev_mem is None
         assert arg.data == 42
         assert arg.nv_data == 42
-        assert not arg.is_pointer
         assert arg.byte_size == 4
         assert arg.direction == CudaArgDirection.input
         assert arg.specified_type is None
         assert arg.ctype == ctypes.c_int
+
+    def test_int_with_type(self) -> None:
+        arg = CudaArg(42, CudaArgType(direction="input", type="uint"))
+        assert not arg.is_pointer
+        assert arg.dev_mem is None
+        assert arg.data == 42
+        assert arg.nv_data == 42
+        assert arg.byte_size == 4
+        assert arg.direction == CudaArgDirection.input
+        assert arg.specified_type is not None
+        assert arg.ctype == ctypes.c_int
+
+    def test_int_sizes(self) -> None:
+        a = CudaArg(0, CudaArgType(direction="input", type="int"))
+        assert a.byte_size == 4
+        a = CudaArg(0, CudaArgType(direction="input", type="uint"))
+        assert a.byte_size == 4
+        a = CudaArg(0, CudaArgType(direction="input", type="int8"))
+        assert a.byte_size == 1
+        a = CudaArg(0, CudaArgType(direction="input", type="uint8"))
+        assert a.byte_size == 1
+        a = CudaArg(0, CudaArgType(direction="input", type="int16"))
+        assert a.byte_size == 2
+        a = CudaArg(0, CudaArgType(direction="input", type="uint16"))
+        assert a.byte_size == 2
+        a = CudaArg(0, CudaArgType(direction="input", type="int32"))
+        assert a.byte_size == 4
+        a = CudaArg(0, CudaArgType(direction="input", type="uint32"))
+        assert a.byte_size == 4
+        a = CudaArg(0, CudaArgType(direction="input", type="int64"))
+        assert a.byte_size == 8
+        a = CudaArg(0, CudaArgType(direction="input", type="uint64"))
+        assert a.byte_size == 8
+
+    def test_int_small_size_warning(self) -> None:
+        with pytest.warns(
+            CudaDataTypeWarning, match="passed int value '-200' too small for 1 bytes of int8"
+        ):
+            CudaArg(-200, CudaArgType(direction="input", type="int8"))
+
+    def test_int_large_size_warning(self) -> None:
+        with pytest.warns(
+            CudaDataTypeWarning, match="passed int value '300' too large for 1 bytes of int8"
+        ):
+            CudaArg(300, CudaArgType(direction="input", type="int8"))
+
+    def test_bytearray(self) -> None:
+        arg = CudaArg(bytearray([1, 2, 3]))
+        assert arg.is_pointer
+        assert arg.dev_mem is not None
+        assert isinstance(arg.data, bytearray)
+        assert arg.data == bytearray([1, 2, 3])
+        assert isinstance(arg.nv_data, int)
+        assert arg.byte_size == 3
+        assert arg.direction == CudaArgDirection.inout
+        assert arg.specified_type is None
+        assert arg.ctype == ctypes.c_void_p
+
+    def test_bytes(self) -> None:
+        arg = CudaArg(bytes([4, 5, 6, 7]))
+        assert arg.is_pointer
+        assert arg.dev_mem is not None
+        assert isinstance(arg.data, bytes)
+        assert arg.data == bytes([4, 5, 6, 7])
+        assert isinstance(arg.nv_data, int)
+        assert arg.byte_size == 4
+        assert arg.direction == CudaArgDirection.inout
+        assert arg.specified_type is None
+        assert arg.ctype == ctypes.c_void_p
