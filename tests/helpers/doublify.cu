2cd4c13bed7b01c9a55e1d7904c0b5baab3bb8d1
Adam Powers <apowers@ato.ms>
2024-10-17 07:40:32 -0700

    test: delint

--- tests/helpers/doublify.cu
+++ tests/helpers/doublify.cu
@@ -1,7 +1,4 @@
 __global__ void doublify(float *a) {
   int idx = threadIdx.x + threadIdx.y * 4;
-  printf("idx %d\n", idx);
-  printf("idx in: %f\n", a[idx]);
   a[idx] *= 2;
-  printf("idx out: %f\n", a[idx]);
 }
